c726746e3059a2f3f61a80d02b14fbad94c18a27
Aitor Soto <aitorsotojimenez99@gmail.com>
2025-01-05 18:48:41 +0100

    Now sends the entire career of the driver in the different teams

--- src/main/java/com/f1/records/repositorys/ConstructorsRepository.java
+++ src/main/java/com/f1/records/repositorys/ConstructorsRepository.java
@@ -15,6 +15,23 @@ public interface ConstructorsRepository extends PagingAndSortingRepository<Const
     @Query("select c from ConstructorDAO c where c.name = ?1")
     ConstructorDAO findConstructorByName(String name);
 
-    @Query("SELECT DISTINCT r.constructorDAO.name FROM ResultDAO r WHERE r.driverId = :id")
-    List<String> findConstructorsOfId(int id);
+    @Query("SELECT c.name " +
+            "FROM ResultDAO r " +
+            "JOIN r.constructorDAO c " +
+            "JOIN r.raceDAO ra " +
+            "WHERE r.driverId = :driverId " +
+            "GROUP BY c.name, ra.year " +  // Agrupamos por nombre del constructor y año
+            "ORDER BY ra.year DESC")
+    List<String> findConstructorsOfId(int driverId);
+
+    /*
+    * SELECT constructors.name
+    * FROM results
+    * INNER JOIN constructors
+    * ON results.constructor_id = constructors.constructor_id
+    * INNER JOIN races
+    * ON results.race_id = races.race_id
+    * WHERE results.driver_id = 4
+    * GROUP BY races.year
+    * ORDER BY races.year desc;*/
 }
